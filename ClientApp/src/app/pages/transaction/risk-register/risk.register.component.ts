a4721e3c341403a3c5a5aa11fb898fc3593dc430
blacksmith18 <ivan@antikode.com>
2018-08-02 02:47:09 +0700

    #risk register 85%

--- ClientApp/src/app/pages/transaction/risk-register/risk.register.component.ts
+++ ClientApp/src/app/pages/transaction/risk-register/risk.register.component.ts
@@ -9,6 +9,7 @@ import { RiskRegisterCtrComponent } from "./modal/risk.register.ctr.component";
 import { RiskRegisterQlComponent } from "./modal/risk.register.ql.component";
 import { RiskRegisterTrtComponent } from "./modal/risk.register.trt.component";
 import * as moment from "moment";
+import * as $ from "jquery";
 import { ToastrService } from "ngx-toastr";
 import { BackendService } from "../../../@core/data/backend.service";
 import { ActivatedRoute, Router } from "@angular/router";
@@ -23,10 +24,82 @@ import { Location } from "@angular/common";
 
 export class RiskRegisterComponent {
 
+  @ViewChild("myForm") private myForm: NgForm;
+
   risk_period : any = moment().format('YYYY');
   register_no : string = 'ASM/ASBD-01';
 
-  @ViewChild("myForm") private myForm: NgForm;
+  selectors : any = {
+    divisions: [],
+    department: [],
+    companyKpi: [],
+    riskImpact: [],
+    riskLevel: [],
+    financialImpact: [{
+      charId: "NEP",
+      description: "Net Profit"
+    },
+    {
+      charId: "REV",
+      description: "Revenue"
+    },
+    {
+      charId: "COF",
+      description: "Cost of revenue"
+    },
+    {
+      charId: "OEX",
+      description: "Operating expenses"
+    },
+    {
+      charId: "CHG",
+      description: "Non operating income/charges"
+    }],
+    qualitativeImpact: []
+  }
+  state : any = {
+    division: '',
+    department: '',
+    companyKpi:[],
+    departmentObjectiveKpi: '',
+    activityBusinessProcess: '',
+    lossEvent:'',
+    cause: '',
+    riskImpact: [],
+    riskLevel : '',
+    notes: '',
+    financialImpact: '',
+    amount: '',
+    operationalImpact: '',
+    loss: '',
+    qualitativeImpact: ''
+  }
+
+  data: any = {
+    division: [],
+    department: [],
+    riskIndicators: [],
+    financialImpact: [{
+      charId: "NEP",
+      description: "Net Profit"
+    },
+    {
+      charId: "REV",
+      description: "Revenue"
+    },
+    {
+      charId: "COF",
+      description: "Cost of revenue"
+    },
+    {
+      charId: "OEX",
+      description: "Operating expenses"
+    },
+    {
+      charId: "CHG",
+      description: "Non operating income/charges"
+    }]
+  }
    
   ctrType = [
     { value: "Preventive", title: "Preventive" },
@@ -464,11 +537,10 @@ export class RiskRegisterComponent {
       //console.log("activatedroute");
       //console.log(url);
     });
-    this.loadData();
+    // this.loadData();
     this.accidentSrc.load(this.dataInput.riskDescription.accidentObj);
     this.controlSrc.load(this.dataInput.currentAction.controls);
     this.treatmentSrc.load(this.dataInput.expectedRisk.treatmentPlanArr);
-
   }
 
   getListRiskIndicator(value) {
@@ -529,6 +601,7 @@ export class RiskRegisterComponent {
 
   loadData() {
     this.service.getreq("TbMRiskIndicators").subscribe(response => {
+      console.log("TbMRiskIndicators", response);
       if (response != null) {
         const data = response;
         //console.log(JSON.stringify(response));
@@ -624,15 +697,59 @@ export class RiskRegisterComponent {
   }
 
   ngOnInit() {
+    $('.select-multiple').select2();
+    //getRouteParams
     this.route.queryParams.subscribe(params => {
-      if (params.draftJson != null) {
-        this.dataInput = JSON.parse(params.draftJson);
-        this.draftData = params;
-        this.accidentSrc.load(this.dataInput.riskDescription.accidentObj);
-        this.controlSrc.load(this.dataInput.currentAction.controls);
-        this.treatmentSrc.load(this.dataInput.expectedRisk.treatmentPlanArr);
-      }
+      console.log("draft Edit");
     });
+    //get Risk Indicator
+    this.service.getreq("TbMRiskIndicators").subscribe(response => {
+      console.log("TbMRiskIndicators", response);
+      this.data.riskIndicators = response;
+      this.selectors.riskImpact = this.data.riskIndicators.filter(object => return (object.flagActive == 'Active' &&  object.condition == 'RTP'));
+      this.selectors.riskLevel = this.data.riskIndicators.filter(object => return (object.flagActive == 'Active' &&  object.condition == 'LVL'));
+    });
+    //get Department
+    this.service.getreq("tbmlibraries").subscribe(response => {
+      this.data.divisions = response.filter(object => return (object.condition == 'DIV'));
+      this.selectors.divisions = this.data.divisions;
+    });
+    //getDivision
+    this.service.getreq("tbmdivdepts").subscribe(response => {
+      this.data.department = response.filter(object=> return (object.kodeDivisi == object.kodeDepartment));
+    });
+    //get Company KPI
+    this.service.getreq("TbMComInputs").subscribe(response => {
+      this.selectors.companyKpi = response.filter(object => object.flagActive == 'Active' && object.condition == 'KPI');
+    });
+    //get financial
+    this.service.getreq("TbMFinancialImpacts").subscribe(response => {
+      // console.log(response);
+      // this.data.financialImpact = response.filter(object=> return (object.kodeDivisi == object.kodeDepartment));
+    });
+    //getQualitative Impact
+    this.service.getreq("TbMQualitativeImpacts").subscribe(response => {
+      // console.log(response);
+    });
+  }
+
+  changeDivision(){
+    this.selectors.department = this.data.department.filter(object=> return (object.kodeDivisi == this.state.division));
+  }
+  changeDepartment(){
+    console.log("department Change to", this.state.department);
+  }
+  changeCompanyKpi(){
+    console.log("CompanyKPI Change to", this.state.companyKpi);
+  }
+  changeRiskImpact(){
+    console.log("RiskImpact Change to", this.state.riskImpact);
+  }
+  changeRiskLevel(){
+    console.log("RiskLevel Change to", this.state.riskLevel);
+  }
+  changeFinancialImpact(){
+    console.log("FinancialImpact Change to", this.state.financialImpact);
   }
 
   ngAfterViewInit() {}
